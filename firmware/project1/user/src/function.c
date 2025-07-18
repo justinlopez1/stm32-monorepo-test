8bba5eea320f1c5892a75e7dcabb0820f9bcf8e9
Justin Lopez New Laptop <justinlopez1@ufl.edu>
2025-07-17 21:24:05 -0400

    testing pre commit again

--- firmware/project1/user/src/function.c
+++ firmware/project1/user/src/function.c
@@ -1,24 +1,26 @@
 #include "function.h"
-#include "cmsis_os.h"
-#include "gpio.h" 
-#include "stm32l4xx_hal.h"
-#include "main.h"
 
-void StartDefaultTask(void const * argument)
+#include "cmsis_os.h"
+#include "gpio.h"
+#include "main.h"
+#include "stm32l4xx_hal.h"
+
+void StartDefaultTask(void const* argument)
 {
-  for(;;)
-  {
-    // toggle led at some point
-    toggle_led();
-    osDelay(200);
-  }
-  /* USER CODE END StartDefaultTask */
+    for (;;)
+    {
+        // toggle led at some point
+        toggle_led();
+        osDelay(200);
+    }
+    /* USER CODE END StartDefaultTask */
 }
 
-void toggle_led() {
-        HAL_GPIO_TogglePin(LD2_GPIO_Port,
-          
-          
-          LD2_Pin)
-          ;
+void toggle_led()
+{
+    HAL_GPIO_TogglePin(LD2_GPIO_Port,
+
+                       LD2_Pin)
+
+        ;
 }
